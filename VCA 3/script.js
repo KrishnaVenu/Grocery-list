ee1e93bd4360b0531dc052a6340a28922d67524c
KrishnaVenu <62461769+KrishnaVenu@users.noreply.github.com>
2020-05-03 14:08:22 +0530

    Add files via upload

---
+++ VCA 3/script.js
@@ -0,0 +1,22 @@
+function ajax(){
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if(this.readyState==4 && this.status ==200){
+        var response=JSON.parse(this.responseText);
+        console.log(response);
+        var Jgrocerylist = response.grocerylist;
+        console.log(Jgrocerylist);
+        var output="";
+        for(var i=0;i<Jgrocerylist.length;i++)
+        output +="<td>" + Jgrocerylist[i].SerialNumber + "</td>";
+        output +="<td>" + Jgrocerylist[i].Name + "</td>";
+        output +="<td>" + Jgrocerylist[i].Quantity + "</td>";
+        output +="<td>" + Jgrocerylist[i].Unit + "</td>";
+        output +="<td>" + Jgrocerylist[i].Department + "</td>";
+        output +="<td>" + Jgrocerylist[i].Notes + "</td>";
+}
+document.getElementById("demo").innerHTML=output;
+};
+xhttp.open("GET","shopping.json",true);
+xhttp.send();
+}
